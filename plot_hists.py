3928148b13ed8833c33f0115f8e30afcf7c419db
nick williams <nick5664@outlook.com>
2019-06-01 06:58:36 -0600

    new ones

--- plot_hists.py
+++ plot_hists.py
@@ -6,7 +6,7 @@ def pull_in_file(f_name):
 
 
 if __name__ == '__main__':
-    thist = pull_in_file('trade_hist1368.txt')
+    thist = pull_in_file('trade_hist1403.txt')
     print(list(thist))
     print(thist.head())
 
