a56c99538e5b97697caddcc409b1bbd3dcf41fe0
nick williams <nick5664@outlook.com>
2019-09-30 20:50:35 -0600

    yeah

--- polo_trainer.py
+++ polo_trainer.py
@@ -162,7 +162,7 @@ class PurpleTrader:
 
     def validate_fitness(self):
         config = self.config
-        genomes = neat.Checkpointer.restore_checkpoint("./pkl_pops/pop-checkpoint-49").population
+        genomes = neat.Checkpointer.restore_checkpoint("./pkl_pops/pop-checkpoint-11").population
         self.epoch_len = 377
         r_start = self.hs.hist_full_size - self.epoch_len-1
         best_g_fit = 0.0
@@ -216,5 +216,5 @@ def run_validation():
 
     task.validate_fitness()
 
-run_training()
-#run_validation()
+#run_training()
+run_validation()
