258d288314e35d7b96cc4f3e4fb61cb7aec5a053
nick williams <nick5664@outlook.com>
2019-10-06 13:46:10 -0600

    gettit

--- brain_trader.py
+++ brain_trader.py
@@ -314,32 +314,37 @@ class PaperTrader:
         sub = Substrate(self.in_shapes, self.out_shapes)
         network = ESNetwork(sub, self.cppn, self.params)
         net = network.create_phenotype_network_nd('paper_net.png')
-        net.reset()
-        for n in range(1, self.hist_depth+1):
-            out = net.activate(active[self.hist_depth-n])
-        #print(len(out))
-        rng = len(out)
+        for n in range(1, self.hd):
+            network.activate(active[self.hd-n])
+        out = network.activate(active[0])
+        for x in range(len(out)):
+            if(z > (self.epoch_len+rand_start)-2):
+                sym = self.hs.coin_dict[x]
+                end_prices[sym] = self.hs.currentHists[sym]['close'][self.epoch_len+rand_start]
+            if(out[x] > .5):
+                buy_signals.append(out[x])
+                buy_syms.append(self.hs.coin_dict[x])
+            if(out[x] < -.5):
+                sell_signals.append(out[x])
+                sell_syms.append(self.hs.coin_dict[x])
         #rng = iter(shuffle(rng))
+        sorted_buys = np.argsort(buy_signals)[::-1]
+        sorted_sells = np.argsort(sell_signals)
         self.reset_tickers()
-        for x in np.random.permutation(rng):
-            sym = self.hs.coin_dict[x]
-            #print(out[x])
-            try:
-                if(out[x] < -.5):
-                    p = self.get_price('BTC_'+sym)
-                    print("selling: ", sym)
-                    self.folio.sell_coin(sym, p)
-                elif(out[x] > .5):
-                    p = self.get_price('BTC_'+sym)
-                    print("buying: ", sym)
-                    self.folio.buy_coin(sym, p)
-                else:
-
-            except:
-                print("error buying or selling")
-            #skip the hold case because we just dont buy or sell hehe
-            end_prices[sym] = self.hs.currentHists[sym]["close"].iloc[-1]
-        
+        try:
+            for x in sorted_sells:
+                sym = sell_syms[x]
+                p = self.get_price('BTC_'+sym)
+                print("selling: ", sym)
+                self.folio.sell_coin(sym, p)
+                #portfolio.sell_coin(sym, self.hs.currentHists[sym]['close'][z])
+            for x in sorted_buys:
+                sym = buy_syms[x]
+                p = self.get_price('BTC_'+sym)
+                print("buying: ", sym)
+                self.folio.buy_coin(sym, p)
+        except:
+            print("error placing order")
         self.trade_hist["date"] = datetime.now()
         self.trade_hist["portfoliovalue"] = self.folio.get_total_btc_value_no_sell(end_prices)[0] 
         self.trade_hist["portfolio"] = self.folio.ledger
