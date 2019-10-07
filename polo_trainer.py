7417504aa92aa255ca61ccf8ab922f235afa971e
Nick Williams <nickwilliamslovescode@gmail.com>
2019-10-07 05:54:07 -0500

    k

--- polo_trainer.py
+++ polo_trainer.py
@@ -184,7 +184,7 @@ class PurpleTrader:
 # Create the population and run the XOR task by providing the above fitness function.
 def run_pop(task, gens):
     #pop = neat.population.Population(task.config)
-    pop = neat.Checkpointer.restore_checkpoint("./pkl_pops/pop-checkpoint-1")
+    pop = neat.Checkpointer.restore_checkpoint("./pkl_pops/pop-checkpoint-85")
     checkpoints = neat.Checkpointer(generation_interval=2, time_interval_seconds=None, filename_prefix='./pkl_pops/pop-checkpoint-')
     stats = neat.statistics.StatisticsReporter()
     pop.add_reporter(stats)
