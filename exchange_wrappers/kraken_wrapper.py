6bf4e24a80095e91ee90e91e5da727289715ba72
nick williams <nick5664@outlook.com>
2019-10-22 05:47:17 -0600

    kraken hist ready

--- exchange_wrappers/kraken_wrapper.py
+++ exchange_wrappers/kraken_wrapper.py
@@ -3,11 +3,13 @@ import time
 import pandas as pd
 import requests
 from datetime import date, timedelta, datetime
+import os
 
 class KrakenWrapper(object):
     endpoints = {
         "trade_assets": "https://api.kraken.com/0/public/AssetPairs",
-        "ohlc_bars": "https://api.kraken.com/0/public/OHLC?pair={0}&since={1}&interval={2}"
+        "ohlc_bars": "https://api.kraken.com/0/public/OHLC?pair={0}&since={1}&interval={2}",
+        "files_path": "./hist_data/kraken_data/"
     }
 
     def __init__(self, key="", secret="", default_lookback = 233, default_lb_interval = 240):
@@ -31,6 +33,27 @@ class KrakenWrapper(object):
         lookback_ts = datetime.today() - timedelta(self.look_back)
         for i in sym_list:
             hist_req = requests.get(self.endpoints["ohlc_bars"].format(i, lookback_ts, self.lb_interval))
-            df = pd.DataFrame(hist_req.json()["result"])
-            df.to_csv("./hist_data/kraken_data/" + i + ".txt")
-            print("saved ", i, " to csv no col labels")
+            results = hist_req.json()["result"][i]
+            with open("./hist_data/kraken_data/"+ i + ".txt", "a") as f:
+                f.write("time,open,high,low,close,vwap,vol\n")
+                for l in range(len(results)):
+                    next_line = results[l]
+                    for x in range(len(next_line)):
+                        f.write(str(next_line[x]))
+                    f.write("\n")
+            print(i, " written")
+
+    def pull_single_sym_hist(self):
+        sym_list = self.get_assets("USD")
+        lookback_ts = datetime.today() - timedelta(self.look_back)
+        sym = sym_list[0]
+        hist_req = requests.get(self.endpoints["ohlc_bars"].format(sym, lookback_ts, self.lb_interval))
+        results = hist_req.json()["result"][sym]
+        for i in range(len(results)):
+            print(results[i])
+
+    def load_hist_files(self):
+        histFiles = os.listdir(os.path.join(os.path.dirname(__file__), '../hist_data/kraken_data'))
+        first_sym = histFiles[0]
+        df = pd.DataFrame().from_csv(self.endpoints["files_path"]+first_sym)
+        print(df.head())
