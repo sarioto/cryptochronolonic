6b1136065d6d3dfed6dbc793101bc27131d2984d
nick williams <nick5664@outlook.com>
2019-12-08 15:01:08 -0700

    newest

--- kraken_trainer.py
+++ kraken_trainer.py
@@ -22,8 +22,8 @@ class PurpleTrader:
     #needs to be initialized so as to allow for 62 outputs that return a coordinate
 
     # ES-HyperNEAT specific parameters.
-    params = {"initial_depth": 3,
-            "max_depth": 4,
+    params = {"initial_depth": 1,
+            "max_depth": 3,
             "variance_threshold": 0.3,
             "band_threshold": 0.3,
             "iteration_level": 3,
