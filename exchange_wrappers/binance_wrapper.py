2cf68b5099c290e74270ae3380f73d06de4e5881
nick williams <nick5664@outlook.com>
2020-02-25 07:21:22 -0700

    got history

--- exchange_wrappers/binance_wrapper.py
+++ exchange_wrappers/binance_wrapper.py
@@ -5,17 +5,24 @@ import pandas as pd
 
 class BinanceUsWrapper(object):
 
-    candlestick_endpoint = "/api/v3/klines"
+    candlestick_endpoint = "/api/v3/klines?symbol={}&interval={}"
     base_endpoint = "https://api.binance.us"
     info_endpoint = "/api/v3/exchangeInfo"
     def __init__(self):
         return
 
-    def get_info(self):
+    def get_symbols(self):
         response = requests.get(self.base_endpoint + self.info_endpoint)
-        print(len(response.json()["symbols"]))
+        return response.json()["symbols"]
+
+    def get_symbol_hist(self):
+        symbol = self.get_symbols()[0]["symbol"]
+        resp = requests.get(self.base_endpoint + self.candlestick_endpoint.format(symbol, "1h"))
+        print(resp.json())
+
+        
 
 biwrap = BinanceUsWrapper()
-biwrap.get_info()
+biwrap.get_symbol_hist()
 
     
