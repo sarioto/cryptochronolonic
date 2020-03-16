574daba535d9457edec0fffc8ec0063a325f21e6
nick williams <nick5664@outlook.com>
2020-03-16 07:13:46 -0600

    lets goooo

--- vol_predict.py
+++ vol_predict.py
@@ -1,15 +1,11 @@
-import numpy as np
+from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 import pandas as pd
 from exchange_wrappers import deribit_wrapper
-
-
-feature_cols = ["high", "low", "open", "close", "volume"]
-hist_wrapper = deribit_wrapper.DeribitWrapper()
-
-df = hist_wrapper.fetch_chart_data()
-features = df[feature_cols].values
-data_set = (features - features.mean(axis=0)) / features.std(axis=0)
 
 
 def multivariate_data(dataset, target, start_index, end_index, history_size,
@@ -32,10 +28,63 @@ def multivariate_data(dataset, target, start_index, end_index, history_size,
 
   return np.array(data), np.array(labels)
 
+def create_time_steps(length):
+  return list(range(-length, 0))
 
-past_history = 720
-future_target = 72
-STEP = 6
+def show_plot(plot_data, delta, title):
+  labels = ['History', 'True Future', 'Model Prediction']
+  marker = ['.-', 'rx', 'go']
+  time_steps = create_time_steps(plot_data[0].shape[0])
+  if delta:
+    future = delta
+  else:
+    future = 0
+
+  plt.title(title)
+  for i, x in enumerate(plot_data):
+    if i:
+      plt.plot(future, plot_data[i], marker[i], markersize=10,
+               label=labels[i])
+    else:
+      plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
+  plt.legend()
+  plt.xlim([time_steps[0], (future+5)*2])
+  plt.xlabel('Time-Step')
+  return plt
+
+def plot_train_history(history, title):
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  epochs = range(len(loss))
+
+  plt.figure()
+
+  plt.plot(epochs, loss, 'b', label='Training loss')
+  plt.plot(epochs, val_loss, 'r', label='Validation loss')
+  plt.title(title)
+  plt.legend()
+
+  plt.show()
+
+TRAIN_SPLIT = 6000
+BUFFER_SIZE = 455
+BATCH_SIZE = 255
+EVALUATION_INTERVAL = 20
+EPOCHS = 16
+tf.random.set_seed(13)
+
+feature_cols = ["volume", "open", "close", "spread"]
+hist_wrapper = deribit_wrapper.DeribitWrapper()
+
+df = hist_wrapper.fetch_chart_data()
+df["spread"] = ( df["high"] - df["low"] )
+features = df[feature_cols].values
+dataset = (features - features.mean(axis=0)) / features.std(axis=0)
+
+past_history = 144
+future_target = 15
+STEP = 5
 
 x_train_single, y_train_single = multivariate_data(dataset, dataset[:, 1], 0,
                                                    TRAIN_SPLIT, past_history,
@@ -55,8 +104,22 @@ val_data_single = val_data_single.batch(BATCH_SIZE).repeat()
 
 # setup model
 single_step_model = tf.keras.models.Sequential()
-single_step_model.add(tf.keras.layers.LSTM(32,
+single_step_model.add(tf.keras.layers.LSTM(89,
                                            input_shape=x_train_single.shape[-2:]))
 single_step_model.add(tf.keras.layers.Dense(1))
 
 single_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(), loss='mae')
+
+for x, y in val_data_single.take(1):
+  print(single_step_model.predict(x).shape)
+
+single_step_history = single_step_model.fit(train_data_single, epochs=EPOCHS,
+                                            steps_per_epoch=EVALUATION_INTERVAL,
+                                            validation_data=val_data_single,
+                                            validation_steps=50)
+
+for x, y in val_data_single.take(3):
+  plot = show_plot([x[0][:, 1].numpy(), y[0].numpy(),
+                    single_step_model.predict(x)[0]], 12,
+                   'Single Step Prediction')
+  plot.show()
